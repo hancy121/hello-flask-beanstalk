b1fbd37b153590e50f43154bb8e23d333e8fb90a
hancy121 <96573674+hancy121@users.noreply.github.com>
2026-09-06 14:23:15 +0530

    Update application.py

--- app/application.py
+++ app/application.py
@@ -1,1 +1,22 @@
+from flask import Flask
 
+application = Flask(__name__)
+
+
+@application.route("/")
+def hello():
+    return """
+    <html>
+        <head>
+            <title>Hello AWS</title>
+        </head>
+        <body>
+            <h1>Hello World!</h1>
+            <p>Flask is running on AWS Elastic Beanstalk.</p>
+        </body>
+    </html>
+    """
+
+
+if __name__ == "__main__":
+    application.run()
