76d14c3be5784524b0f6a6acf5458822a7463fbd
hancy121 <96573674+hancy121@users.noreply.github.com>
2026-09-06 14:24:36 +0530

    Update app.py

--- infrastructure/app.py
+++ infrastructure/app.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python3
 
+import aws_cdk as cdk
+
+from hello_beanstalk.hello_beanstalk_stack import HelloBeanstalkStack
+
+
+app = cdk.App()
+
+HelloBeanstalkStack(
+    app,
+    "HelloBeanstalkStack",
+)
+
+app.synth()
